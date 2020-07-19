87380afc21e795a7b423f980603d6b1ea542689a
Dennis <>
2020-07-19 23:46:18 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -71,19 +71,76 @@ def __init__():
 def detectHands(frame):
     global detection_graph, sess, lhands
     boxes, scores = detector_utils.detect_objects(
-        image_np, detection_graph, sess)
+        frame, detection_graph, sess)
 
     hands = [None, None]
 
     for x in range(2):
         if scores[x] > 0.2:
             hands[x] = boxes[x]
-
-    if hands[0] is not None and hands[1] is not None:
-        pass
-    else:
-        pass
+            hands[x][0] = int(hands[x][0] * 640)
+            hands[x][1] = int(hands[x][1] * 480)
+            hands[x][2] = int(hands[x][2] * 640)
+            hands[x][3] = int(hands[x][3] * 480)
     
-    lhands = hands.copy()
+    isNone = [hands[0] is not None, hands[1] is not None]
+    lisNone = [lhands[0] is not None, lhands[1] is not None]
+
+    if isNone[0] and isNone[1]:
+        acc = [None, None]
+        if lisNone[0]:
+            acc[0] = np.mean(hands[0] == lhands[0])
+        if lisNone[1]:
+            acc[1] = np.mean(hands[0] == lhands[1])
+        
+        if lisNone[0] and lisNone[1]:
+            if acc[0] < acc[1]:
+                hands = hands[::-1]
+    elif isNone[0] or isNone[1]:
+        acc = [None, None]
+        hand = None
+
+        if isNone[0]:
+            hand = 0
+        elif isNone[1]:
+            hand = 1
+        else:
+            raise Exception("ArgumentError while Loop")
+
+        if lisNone[0]:
+            acc[0] = np.mean(hands[hand] == lhands[0])
+        if lisNone[1]:
+            acc[1] = np.mean(hands[hand] == lhands[1])
+        
+        if lisNone[0] and lisNone[1]:
+            if acc[0] > acc[1]:
+                if hand == 1:
+                    hands = hands[::-1]
+            else:
+                if hand == 0:
+                    hands = hands[::-1]
+        # else:
+        #     if lisNone[hand]:
+        #         if acc[hand] < 0.6:
+        #             hands = hands.reverse()
+
+    lhands = hands
 
     return hands
+
+
+__init__()
+
+cam = cv2.VideoCapture('http://192.168.178.195:4747/mjpegfeed?640x480')
+
+while True:
+    img = cam.read()[1]
+    boxes = detectHands(img)
+
+    if boxes[0] is not None:
+        cv2.rectangle(img, (boxes[0][0], boxes[0][1]), (boxes[0][2], boxes[0][3]), (0, 0, 255))
+    if boxes[1] is not None:
+        cv2.rectangle(img, (boxes[1][0], boxes[1][1]), (boxes[1][2], boxes[1][3]), (0, 0, 255))
+    
+    cv2.imshow("Output", img)
+    cv2.waitKey(1)
